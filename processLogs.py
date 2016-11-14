d8d903f59bf9bdc2a5402b77123120165f728511
Mides <michel.descouens@gmail.com>
2016-11-14 19:05:39 +0100

    Refactoring method ProcessLogs()

--- processLogs.py
+++ processLogs.py
@@ -7,7 +7,7 @@
 #      process the www.geocaching.com logs for a given geocacher
 #      generate a summary in XML format
 #
-# Copyright GarenKreiz at  geocaching.com or on  YouTube 
+# Copyright GarenKreiz at  geocaching.com or on  YouTube
 # Auteur    GarenKreiz sur geocaching.com ou sur YouTube
 #
 # Licence:
@@ -29,27 +29,27 @@ import re
 import os
 import sys
 import time
-import random
 import locale
 import urllib2
-import datetime
+
 
 #os.environ['LC_ALL'] = 'fr_FR.UTF-8'
 #locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 locale.setlocale(locale.LC_ALL, '')
 
 # default title and description of the logbook (should be in logbook_header.xml)
-bookTitle="""<title>Titre Ã  paramÃ©trer<br/> Customizable title</title>"""
-bookDescription="""<description>Description du journal - Logbook description - Fichier Ã  modifier : logbook_header.xml - Modify file : logbook_header.xml</description>"""
+bookTitle="""<title>Titre à paramétrer<br/> Customizable title</title>"""
+bookDescription="""<description>Description du journal - Logbook description - Fichier à modifier : logbook_header.xml - Modify file : logbook_header.xml</description>"""
 
 
 # jump to a given pattern while analysing a file
 def skipTo(fIn, searchString):
-    p = re.compile(searchString)
-    l = fIn.readline()
-    while l <> '' and (not p.search(l)):        
-        l = fIn.readline()
-    return l
+    pattern = re.compile(searchString)
+    line = fIn.readline()
+    while line <> '' and (not pattern.search(line)):
+        line = fIn.readline()
+    #print line
+    return line
 
 def normalizeDate(date):
     date = re.sub('[-. ]+','/',date)
@@ -63,17 +63,16 @@ def normalizeDate(date):
         d,y = y,int(d)+2000
     date = '%02d/%02d/%02d'%(int(y),int(m),int(d))
     return date
-    
+
 
 def formatDate(date):
-    (year,month,day) = date.split('/')
     strTime = date+" 00:00:01Z"
     t=0
     try:
         t = int(time.mktime(time.strptime(strTime, "%Y/%m/%d %H:%M:%SZ")))
     except:
         pass
-    
+
     date = time.strftime('%A %d %B %Y', time.localtime(t))
     date = date.decode(locale.getpreferredencoding()).encode('utf8')
     date = re.sub(' 0',' ',date)
@@ -94,7 +93,7 @@ class Logbook:
         self.endDate = endDate
         self.refresh = refresh
         self.excluded = excluded
-        
+
         self.nDates = 0          # number of processed dates
         self.nLogs = 0           # number of processed logs
 
@@ -112,7 +111,7 @@ class Logbook:
             urlLog = 'track'
         self.fXML.write('<post>%s | http://www.geocaching.com/%sdetails.aspx?guid=%s |'%(title,url,cacheID))
         self.fXML.write('%s | http://www.geocaching.com/%s/log.aspx?LUID=%s</post>\n'%(status,urlLog,logID))
-                   
+
         tBegin = data.find('_LogText">')
         if tBegin > 0:
             tBegin += len('_LogText">')
@@ -124,11 +123,11 @@ class Logbook:
             print "!!!! Log unavailable",logID
             return
         if self.localImages:
-            text = re.sub('src="/images/','src="Images/',text)                
+            text = re.sub('src="/images/','src="Images/',text)
         else:
-            text = re.sub('src="/images/','src="http://www.geocaching.com/images/',text)    
+            text = re.sub('src="/images/','src="http://www.geocaching.com/images/',text)
         self.fXML.write('<text>%s</text>\n'%text)
-        
+
         listPanoramas=[]
         listImages=[]
 
@@ -145,7 +144,7 @@ class Logbook:
                     print '!!!! Bad image:',src
                     p = data.find('<img ',p+4)
                     continue
-                
+
                 # normalize form : http://img.geocaching.com/cache/log/display/*.jpg
                 if self.localImages:
                     src = re.sub('.*(thumb|display)','Images',src)     # to use if there is a local cache of images
@@ -165,11 +164,11 @@ class Logbook:
                     tEnd = patternE.search(data,tBegin).start()  # end of tag
                     caption = re.sub('<[^>]*>','',data[tBegin:tEnd])
                 caption = caption.strip(' \n\r\t')
-                
+
                 # images with "panorama" or "panoramique" in the captin are supposed to be wide pictures
                 if re.search('panoram', caption, re.IGNORECASE):
                     src = re.sub('/log/display/','/log/',src)     # use full size image for panorama
-                    
+
                     if not (src,caption) in listPanoramas:
                         listPanoramas.append((src,caption))
                 else:
@@ -182,10 +181,10 @@ class Logbook:
                     images[logID].append((src,caption))
                 except:
                     images[logID] = ((src,caption))
-                    
+
                 # goto next image
                 p = data.find('<img alt=',p+1)
-                
+
             # panoramas are displayed after the other images
             # each on a separate line
             if listPanoramas <> []:
@@ -193,10 +192,10 @@ class Logbook:
                     self.fXML.write("<pano>%s<height>480</height><width>640</width><comment>%s</comment></pano>\n"%(src, caption))
 
         self.nLogs += 1
-        
-        # identifying logs without image            
+
+        # identifying logs without image
         try:
-            foo = images[logID][0]
+            images[logID][0]
         except:
             print "!!!! Log without image:",logID,day,title,'>>>',status
 
@@ -218,49 +217,33 @@ class Logbook:
         resu = None
         while resu <> '':
             # to be improved!!!!
-            typeLog = skipTo(self.fIn,'/images/logtypes')
+            typeLog = skipTo(self.fIn,'height="16" alt=')
             if typeLog == '':
-                # end of file
-                break
-            typeLog = re.sub('.*alt="','',typeLog)
-            typeLog = re.sub('".*','',typeLog)
-            typeLog = re.sub('[\n\r]*','',typeLog)
-
+                break # end of file
+            typeLog = re.search('title=\"(.*)\"',typeLog).group(1).replace('\r\n','')
             resu = skipTo(self.fIn,'<td>')
             resu = skipTo(self.fIn,'<td>')
             # parse date (numerical format only) and transform in canonical form yyyy/mm/dd
             dateLog = self.fIn.readline().strip()
             dateLog = normalizeDate(dateLog)
-            
             resu = skipTo(self.fIn,'details')
-            if (resu.find('cache_details') > 1):
-                logNature = 'C'    # log for a cache
-            else:
-                logNature = 'T'    # log for a trackable
-            resu = re.sub('.*guid=','',resu)
-            resu = re.sub('[\n\r]','',resu)
-            cacheLog = re.sub('".*','',resu)
-            nameLog = re.sub('</a>.*','',resu)
-            nameLog = re.sub('</span>','',nameLog)
-            nameLog = re.sub('.*>','',nameLog)
+            logNature =  ('C' if resu.find('cache_details') > 1 else 'T') # C for Cache and T for trackbale
+            cacheLog = re.search('guid=(.*)\" class',resu).group(1)
+            titleCache = re.search('</a> <a(.*)?\">(.*)</a>',resu).group(2)
             resu = skipTo(self.fIn,'log.aspx')
-            idLog = re.sub('.*LUID=','',resu)
-            idLog = re.sub('".*','',idLog)
-            idLog = re.sub('[ \n\r]','',idLog)
-
+            idLog = re.search('LUID=(.*)\" target',resu).group(1)
             # keeping the logs that are not excluded by -x option
             keep = True
             for typeExclude in self.excluded:
                 keep = keep & (re.search(typeExclude,typeLog,re.IGNORECASE) < 0)
             if keep and idLog <> '':
-                logs[idLog] = (dateLog,cacheLog,nameLog,typeLog,logNature)
+                logs[idLog] = (dateLog,cacheLog,titleCache,typeLog,logNature)
                 try:
-                    days[dateLog].append((idLog,cacheLog,nameLog,typeLog,logNature))
+                    days[dateLog].append((idLog,cacheLog,titleCache,typeLog,logNature))
                 except:
-                    days[dateLog] = [(idLog,cacheLog,nameLog,typeLog,logNature)]
+                    days[dateLog] = [(idLog,cacheLog,titleCache,typeLog,logNature)]
                 if self.verbose:
-                    print "%s|%s|%s|%s|%s|%s"%(idLog,dateLog,cacheLog,nameLog,typeLog,logNature)
-            
+                    print "%s|%s|%s|%s|%s|%s"%(idLog,dateLog,cacheLog,titleCache,typeLog,logNature)
         dates = days.keys()
         dates.sort()
         for d in dates:
@@ -271,11 +254,11 @@ class Logbook:
                 continue
             self.nDates += 1
             self.fXML.write('<date>%s</date>\n'%formatDate(d))
-          
+
             dayLogs = days[d]
             dayLogs.reverse()
             for (l,c,t,s,logNature) in dayLogs:
-                # logId, cacheId or tbID, title, type, nature 
+                # logId, cacheId or tbID, title, type, nature
                 # building a local cache of the HTML page of each log
                 # directory: Logs and 16 sub-directories based on the first letter
                 if logNature == 'C':
@@ -283,7 +266,7 @@ class Logbook:
                     dir = 'Logs'
                 else:
                     url = 'track'
-                    dir = 'LogsTB'   # dedicated directory for TB logs as ID may be reused between TB and cache logs 
+                    dir = 'LogsTB'   # dedicated directory for TB logs as ID may be reused between TB and cache logs
                 dir = dir + '/_%s_/'%l[0]
                 if not os.path.isfile(dir+l) or self.refresh:
                     if not os.path.isdir(dir):
@@ -302,46 +285,46 @@ class Logbook:
                     f.close()
                 # grabbing information from the log page
                 self.parseLog(data,d,l,c,t,s,logNature)
-                
+
         self.fXML.write('<date>Source : GarenKreiz/Geocaching-Journal @ GitHub (CC BY-NC 3.0 FR)</date>\n')
         print 'Logs: ',self.nLogs,'/',len(logs), 'Days:',self.nDates,'/',len(dates)
-        print 'Result file:', self.fNameOutput 
+        print 'Result file:', self.fNameOutput
 
 if __name__=='__main__':
     def usage():
-          print 'Usage: python processLogs.py [-q|--quiet] [-l|--local-images] geocaching_logs.html logbook.xml'
-          print '    or python processLogs.py [-q|--quiet] [-l|--local-images] geocaching_logs.html logbook.html'
-          print ''
-          print '   geocaching_logs.html'
-          print '       dump of the web page containing all you logs (HTML only)'
-          print '       sauvegarde de la page contenant tous vos logs (HTML uniquement)'
-          print '   logbook.xml'
-          print '       content of all log entries with reference to pictures'
-          print '       contenu de tous les logs avec references aux images'
-          print '   logbook.html'
-          print '       web page with all logs and images (using xml2print.py)'
-          print '       page web avec tous les logs et images (utilise xml2print.py)'
-          print ''
-          print '   -q|--quiet'
-          print '       less verbose console output'
-          print '       execution du programme moins verbeuse'
-          print '   -l|--local-images'
-          print '       use local images in directory Images (previously downloaded for example using "wget")'
-          print '       utilisation d\'une copie locale des images dans le repertoire Images (telechargees par exemple avec "wget")'
-          print '   -s|--start startDate'
-          print '       start processing log at date startDate (included, format YYYY/MM/DD)'
-          print '       commence le traitement des logs Ã  partir de la date startDate incluse (format AAAA/MM/JJ)'
-          print '   -e|--end endDate'
-          print '       stop processing log after date endDate (format YYYY/MM/DD)'
-          print '       arrete le traitement des logs apres la date endDate (format AAAA/MM/JJ)'
-          print '   -r|--refresh'
-          print '       refresh local cache of logs (to use when the log was changed or pictures were added)'
-          print '       rafraichit la version locale des journaux (a utiiser si des modifications ont ete faites ou des photos ont ete ajoutees'
-          print '   -x|--exclude'
-          print '       exclude a given log type, can be repeated (for example "-x update -x disable")'
-          print '       exclusion de certains types de log, par recherche de chaine de caractere (par exemple "-x update -x disable")'
-          
-          sys.exit()
+        print 'Usage: python processLogs.py [-q|--quiet] [-l|--local-images] geocaching_logs.html logbook.xml'
+        print '    or python processLogs.py [-q|--quiet] [-l|--local-images] geocaching_logs.html logbook.html'
+        print ''
+        print '   geocaching_logs.html'
+        print '       dump of the web page containing all you logs (HTML only)'
+        print '       sauvegarde de la page contenant tous vos logs (HTML uniquement)'
+        print '   logbook.xml'
+        print '       content of all log entries with reference to pictures'
+        print '       contenu de tous les logs avec references aux images'
+        print '   logbook.html'
+        print '       web page with all logs and images (using xml2print.py)'
+        print '       page web avec tous les logs et images (utilise xml2print.py)'
+        print ''
+        print '   -q|--quiet'
+        print '       less verbose console output'
+        print '       execution du programme moins verbeuse'
+        print '   -l|--local-images'
+        print '       use local images in directory Images (previously downloaded for example using "wget")'
+        print '       utilisation d\'une copie locale des images dans le repertoire Images (telechargees par exemple avec "wget")'
+        print '   -s|--start startDate'
+        print '       start processing log at date startDate (included, format YYYY/MM/DD)'
+        print '       commence le traitement des logs à partir de la date startDate incluse (format AAAA/MM/JJ)'
+        print '   -e|--end endDate'
+        print '       stop processing log after date endDate (format YYYY/MM/DD)'
+        print '       arrete le traitement des logs apres la date endDate (format AAAA/MM/JJ)'
+        print '   -r|--refresh'
+        print '       refresh local cache of logs (to use when the log was changed or pictures were added)'
+        print '       rafraichit la version locale des journaux (a utiiser si des modifications ont ete faites ou des photos ont ete ajoutees'
+        print '   -x|--exclude'
+        print '       exclude a given log type, can be repeated (for example "-x update -x disable")'
+        print '       exclusion de certains types de log, par recherche de chaine de caractere (par exemple "-x update -x disable")'
+
+        sys.exit()
 
     import getopt
 
@@ -356,7 +339,7 @@ if __name__=='__main__':
     endDate = None
     refresh = False
     excluded = []
-    
+
     for opt, arg in opts:
         if opt == '-h':
             usage()
@@ -369,7 +352,7 @@ if __name__=='__main__':
         elif opt == "-s":
             if len(arg.split('/')) <> 3:
                 print "!!! Bad start date format, use YYYY/MM/DD"
-                print "!!! Format de date de dÃ©but faux, utiliser AAAA/MM/JJ"
+                print "!!! Format de date de début faux, utiliser AAAA/MM/JJ"
                 sys.exit(1)
             startDate = arg
         elif opt == "-e":
@@ -382,7 +365,7 @@ if __name__=='__main__':
             excluded.append(arg)
 
     print "Excluded:",excluded
-    
+
     if len(args) == 2:
         if re.search(".xml", args[0], re.IGNORECASE):
             xmlFile = args[0]
@@ -396,12 +379,11 @@ if __name__=='__main__':
             Logbook(args[0],xmlFile,verbose,localImages,startDate,endDate,refresh,excluded).processLogs()
 
         # second phase : from XML to generated HTML
+        '''
         if re.search(".htm[l]*",args[1], re.IGNORECASE):
             import xml2print
             xml2print.xml2print(xmlFile,args[1])
-
+        '''
         print "That's all folks!"
     else:
         usage()
-        
-          
